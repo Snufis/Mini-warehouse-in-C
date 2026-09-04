fb4ec9e3154088a249b6133b8a4322235d3559e8
Snufis <SkurwielW3@protonmail.com>
2026-09-04 23:41:02 +0200

    added show + more infrastructure

--- main.c
+++ main.c
@@ -2,35 +2,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(void){
-    struct LinkedList {
+struct LinkedList {
         int id;
         char name[50];
         int quantity;
         struct LinkedList *next;
-    };
+};
+
+void FunctionShow(struct LinkedList *head);
+
+
+
+int main(void){
+    
     //temp
     struct LinkedList Bananas;
     struct LinkedList Apples;
     struct LinkedList Grapes;
 
     Bananas.id = 1;
-    strcpy(Bananas.name, "Mouse");
+    strcpy(Bananas.name, "Bananas");
     Bananas.quantity = 50;
     Bananas.next = &Apples;
 
     Apples.id = 2;
-    strcpy(Apples.name, "Mouse");
+    strcpy(Apples.name, "Apples");
     Apples.quantity = 30;
     Apples.next = &Grapes;
 
     Grapes.id = 3;
-    strcpy(Grapes.name, "Mouse");
+    strcpy(Grapes.name, "Grapes");
     Grapes.quantity = 20;
     Grapes.next = NULL;
 
     
     while (1){
+        printf("1.show\n");
+        printf("2.add\n");
+        printf("3.del\n");
+        printf("4.change\n");
+        printf("5.quit\n");
 
+        char action[20] ="";
+
+        scanf("%19s",action);
+
+        if (strcmp(action, "show")==0){
+            FunctionShow(&Bananas);
+        }
+    }
+}
+
+
+
+void FunctionShow(struct LinkedList *head){
+    struct LinkedList *current = head;
+    printf("id | name | quantity\n");
+    while (current != NULL) {
+        printf("%d ",current->id);
+        printf("%s ",current->name);
+        printf("%d\n",current->quantity);
+        current = current->next;
     }
 }
