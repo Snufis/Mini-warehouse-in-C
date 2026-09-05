e7ede648ac095285d1ba3d229892c4db29784304
Snufis <SkurwielW3@protonmail.com>
2026-09-06 00:19:32 +0200

    Delete function + added dynamic linkedlist

--- main.c
+++ main.c
@@ -10,45 +10,48 @@ struct LinkedList {
 };
 
 void FunctionShow(struct LinkedList *head);
-
+void FunctionDelete(struct LinkedList *head);
 
 
 int main(void){
     
     //temp
-    struct LinkedList Bananas;
-    struct LinkedList Apples;
-    struct LinkedList Grapes;
+    struct LinkedList *Bananas = malloc(sizeof(struct LinkedList));
+    struct LinkedList *Apples = malloc(sizeof(struct LinkedList));;
+    struct LinkedList *Grapes = malloc(sizeof(struct LinkedList));;
 
-    Bananas.id = 1;
-    strcpy(Bananas.name, "Bananas");
-    Bananas.quantity = 50;
-    Bananas.next = &Apples;
+    Bananas->id = 1;
+    strcpy(Bananas->name, "Bananas");
+    Bananas->quantity = 50;
+    Bananas->next = Apples;
 
-    Apples.id = 2;
-    strcpy(Apples.name, "Apples");
-    Apples.quantity = 30;
-    Apples.next = &Grapes;
+    Apples->id = 2;
+    strcpy(Apples->name, "Apples");
+    Apples->quantity = 30;
+    Apples->next = Grapes;
 
-    Grapes.id = 3;
-    strcpy(Grapes.name, "Grapes");
-    Grapes.quantity = 20;
-    Grapes.next = NULL;
+    Grapes->id = 3;
+    strcpy(Grapes->name, "Grapes");
+    Grapes->quantity = 20;
+    Grapes->next = NULL;
 
     
     while (1){
-        printf("1.show\n");
-        printf("2.add\n");
-        printf("3.del\n");
-        printf("4.change\n");
-        printf("5.quit\n");
+        printf("-show\n");
+        printf("-add\n");
+        printf("-del\n");
+        printf("-change\n");
+        printf("-quit\n");
 
         char action[20] ="";
 
         scanf("%19s",action);
 
         if (strcmp(action, "show")==0){
-            FunctionShow(&Bananas);
+            FunctionShow(Bananas);
+        }
+        if (strcmp(action,"del") == 0){
+            FunctionDelete(Bananas);
         }
     }
 }
@@ -64,4 +67,29 @@ void FunctionShow(struct LinkedList *head){
         printf("%d\n",current->quantity);
         current = current->next;
     }
+}
+
+void FunctionDelete(struct LinkedList *head){
+    int id;
+    
+    printf("id to delete: \n");
+    scanf("%d", &id);
+
+    struct LinkedList *previous = head;
+    struct LinkedList *current = head->next;
+//problem with deleting first one
+    while (current != NULL) {
+        if(current->id == id) {
+            previous->next = current->next;
+            free(current);
+
+            printf("product with %d id was deleted", id);
+            return;
+        }
+
+        previous = current;
+        current = current->next;
+    }
+    printf("product not found");
+
 }
